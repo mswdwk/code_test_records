ff94e1223bb11fc5b773f7d060d3a73d54907129
mswdwk <mswdwk@outlook.com>
2018-04-28 11:12:02 +0800

    modify launch_tcp_bbr

--- shell/ubuntu/launch_tcp_bbr.sh
+++ shell/ubuntu/launch_tcp_bbr.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 KernelVersion=`uname -r`
+
 major=`echo $KernelVersion|cut -d '.' -f 1`
 minor=`echo $KernelVersion|cut -d '.' -f 2`
+
+enable_tcp_bbr()
+{
+	sysctl net.core.default_qdisc=fq
+	sysctl net.ipv4.tcp_congestion_control=bbr
+}
+
 if [  $major -ge 4 ] && [ $minor -ge 9 ];then
-sysctl net.core.default_qdisc=fq
-sysctl net.ipv4.tcp_congestion_control=bbr
+	enable_tcp_bbr
 else
 	echo "Kernel Version >=4.9 is needed!"
 fi
-#sysctl -p
+
+disable_tcp_bbr()
+{
+	sed -i '/net\.core\.default_qdisc=fq/d' /etc/sysctl.conf
+	sed -i '/net\.ipv4\.tcp_congestion_control=bbr/d' /etc/sysctl.conf
+	sysctl -p
+}
