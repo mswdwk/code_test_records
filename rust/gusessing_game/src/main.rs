e1281d9a7a82d8d333baf296d29b50fa8652a274
mswdwk <mswdwk@outlook.com>
2018-05-23 15:35:09 +0800

    format rust codes twice

--- rust/gusessing_game/src/main.rs
+++ rust/gusessing_game/src/main.rs
@@ -25,9 +25,9 @@ fn main() {
         println!("You guessed: {}", guess);
     
         match guess.cmp(&secret_number){
-            Ordering::Less 		=> println!("Too small!"),
-            Ordering::Greater	=> println!("Too big!"),
-            Ordering::Equal		=> {println!("You win!");break;}
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal     => {println!("You win!");break;}
         } 
     }
 }
