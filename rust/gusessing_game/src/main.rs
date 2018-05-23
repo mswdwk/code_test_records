b747dc0e2bfc6428ab67c4ba68da8326f588d308
mswdwk <mswdwk@outlook.com>
2018-05-23 15:32:37 +0800

    format rust codes

--- rust/gusessing_game/src/main.rs
+++ rust/gusessing_game/src/main.rs
@@ -4,31 +4,30 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number! From 1 to 100.");
     let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("The secret number is:{}",secret_number); 
-  loop{  
-	println!("Please input your guess.");
+    //println!("The secret number is:{}",secret_number); 
+    loop{
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-	if guess.trim() == "quit" {break;}
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        if guess.trim() == "quit" { break;}
 
-	let guess:u32 = match guess.trim().parse(){
-		Ok(num) => num,
-		Err(_)  => {
-		println!("Please type a number!");continue;}
-	};
-		//.expect("Please type a number!");
+        let guess:u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_)  => {println!("Please type a number!");continue;}
+        };
+            //.expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
     
-		match guess.cmp(&secret_number){
-			Ordering::Less 		=> println!("Too small!"),
-			Ordering::Greater	=> println!("Too big!"),
-			Ordering::Equal		=> {println!("You win!");break;}
-		} 
-	}
+        match guess.cmp(&secret_number){
+            Ordering::Less 		=> println!("Too small!"),
+            Ordering::Greater	=> println!("Too big!"),
+            Ordering::Equal		=> {println!("You win!");break;}
+        } 
+    }
 }
