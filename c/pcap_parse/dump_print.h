ec231d412e382805c99bb5e3a1494e51962e8041
mswdwk <mswdwk@outlook.com>
2018-06-15 17:45:47 +0800

    del pcap parser demo

--- c/pcap_parse/dump_print.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DUMP_PRINT_H
-#define _DUMP_PRINT_H
-#include <stdio.h>
-
-void dump_print(char* comment,int buf_len, void *buf);
-void dump_fprintf(FILE*fp,char* comment,int buf_len, void *buf);
-
-#define pdg(fmt,arg...) printf("[%s %d]"fmt,__func__,__LINE__,##arg)
-#endif
--- c/pcap_parse/hash.h
+++
@@ -1,3 +0,0 @@
-#include "basic_data_type.h"
-void init_hash();
-u_int mkhash (u_int , u_short , u_int , u_short);
