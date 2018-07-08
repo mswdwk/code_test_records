eae074817fc79b95f050c6d2feaf4b5bc8ae02f2
mswdwk <mswdwk@outlook.com>
2018-07-08 14:51:50 +0800

    add time display in python plot
    Change-Id: I7fdc2535501021544aaf8bb388f792dc5615c8ad

--- python/plot_multi_pic_with_number.py
+++ python/plot_multi_pic_with_number.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+ 
+# 格式化成2016-03-20 11:45:39形式
+#print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
 
 for i in range(0,10):
 	ax = plt.subplot(111)
@@ -13,5 +20,7 @@ for i in range(0,10):
 	print "i=",i,",",name
 	#plt.text(1, 1, "hell", size=1, ha='center', va='center',color=0x00ff0000)
 	plt.text(1.5, 1, i, size=72, ha='center', va='center',color='g')
+	current_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
+	plt.text(2.5, -1.5, current_time, size=36, ha='center', va='center',color='r')
 	plt.savefig(name)
 	plt.close()
