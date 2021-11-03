0a0b25c110538c0cb483b1ab7a8683c07ff78a2e
mswdwk <mswdwk@outlook.com>
2021-11-03 23:50:06 +0800

    refactor: rich tcp server/client rust codes

--- rust/tcp_client_demo/src/main.rs
+++ rust/tcp_client_demo/src/main.rs
@@ -1,29 +1,43 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
-use std::io::stdin;
-use std::thread;
+// use std::io::stdin;
+use std::{thread, time};
+//use std::borrow::Borrow;
+
 fn main() {
     let mut stream=TcpStream::connect("127.0.0.1:8080").unwrap();
     stream.set_nodelay(true).unwrap();
-    loop{
-        let mut test_stream=stream.try_clone().unwrap();
-        thread::spawn(move||{
-            loop{
-                let mut buffer=[0;1024];
-                let num=test_stream.read(&mut buffer[..]).unwrap();
-                println!("{:?}",num);
-                if num==0{
-                    println!("连接关闭了");
-                    break;
-                }
-                let result=std::str::from_utf8(&buffer[0..num]);
-                print!("{}",result.unwrap());
+    //loop{
+    let mut test_stream=stream.try_clone().unwrap();
+    if let Err(e) = thread::Builder::new().name("test_thread".into()).stack_size(819200).spawn(move||{
+        loop{
+            let mut buffer=[0;1024];
+            let num= test_stream.read(&mut buffer[..]).unwrap();
+            println!("thread name[{:?}] recv data len: {:?}",thread::current().name().unwrap_or(""),num);
+            if num==0{
+                println!("server 连接关闭了");
+                break;
             }
-        });
-        let mut response=String::new();
-        stdin().read_line(&mut response).ok().expect("nothing");
-        //print!("{}",response);
-        stream.write(response.as_bytes()).unwrap();
+            let rcv_str= String::from_utf8_lossy(&buffer[0..num]);
+            println!("recv data raw:{:?}",&buffer[0..num]);
+            println!("recv data str: {}",rcv_str);
+
+        }
+    }){
+        println!("create thread error ! {}",e.to_string());
+    }
+
+    let mut count = 0;
+    let mut response;//= String::from("hello world, ");
+    loop {
+        // stdin().read_line(&mut response).ok().expect("nothing");
+        // print!("{}",response);
+        count += 1;
+        response = format!("Hello World, this is {} times",count);
+        // response.push_str(count.to_string().as_str());
+        let send_bytes = stream.write(response.as_bytes()).unwrap();
+        println!("send_bytes {}",send_bytes);
+        thread::sleep(time::Duration::from_secs(2));
     }
     /*
     loop{
--- rust/tcp_server_demo/src/main.rs
+++ rust/tcp_server_demo/src/main.rs
@@ -1,12 +1,12 @@
 use std::net::TcpListener;
 use std::{thread, time};
-use std::net::TcpStream;
+//use std::net::TcpStream;
 use std::io::prelude::*;
-use std::io::stdin;
+// use std::io::stdin;
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::net::Shutdown;
-use std::sync::atomic::{AtomicUsize, Ordering};
+// use std::net::Shutdown;
+// use std::sync::atomic::{AtomicUsize, Ordering};
 
 fn main() {
     let listener=TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -19,18 +19,53 @@ fn main() {
             {
                 let mut  v = clild_num.lock().unwrap();
                 *v +=1;
-                println!("{}", *v);
+                println!("current client count: {}", *v);
             }
-            println!("come in !!");
+            println!("client come in !!");
             let mut stream = base_stream;
-            println!("from {:?}",stream.peer_addr());
-            let mut bytes: Vec<u8> = Vec::new();;
-            stream.read(&mut bytes);
+            println!("client from {:?}, set block {:?}",stream.peer_addr(),stream.set_nonblocking(false));
+
+            const BUFSIZE:u32 = 128;
+            let mut v = Vec::new();
+            println!("recv_buf capacity: {}", v.capacity());
+            v.resize(BUFSIZE as usize,0 as u8); // let v has data, and length eq BUFSIZE
+            v.fill(0); // fill v with 0 ,position from 0 to BUFSIZE
+            println!("recv_buf capacity: {} {}", v.capacity(),v.len());
+            let mut v2 = vec![0 as u8;BUFSIZE as usize];
+            let mut s = [0 as u8;BUFSIZE as usize ] ;
+            let kind = 3;
+            let recv_buf= if 1 == kind {
+                v.as_mut_slice()
+            } else if 2 == kind {
+                v2.as_mut_slice()
+            } else {
+                &mut s
+            };
+            println!("len {}",recv_buf.len());
+            let mut count:u32 = 0;
             loop {
-                stream.write(String::from("1").as_bytes());
-                thread::sleep(time::Duration::from_secs(2));
+                match stream.read( recv_buf) {
+                    Ok(0) => {
+                        thread::sleep(time::Duration::from_secs(2));
+                    }
+                    Ok(n ) => {
+                        //stream.read_to_end(&mut recv_buf);
+                        count+=1;
+                        // stream.write(&count.to_be_bytes());
+                        let mut send_bytes = stream.write(count.to_string().as_bytes()).unwrap_or(0);
+                        // stream.write(String::from("1").as_bytes());
+                        send_bytes += stream.write(&recv_buf[0..n]).unwrap_or(0);
+                        println!("recv {} times, data len {}, raw data:{:?}",count, n, recv_buf);
+                        println!("send data len {}",send_bytes);
+                        recv_buf.fill(0);
+                    }
+                    Err(e) => {
+                        thread::sleep(time::Duration::from_secs(2));
+                        println!("read error: {}",e.to_string());
+                    }
+                }
             }
-            println!("come end !!");
+            //println!("come end !!");
         });
     }
 }
