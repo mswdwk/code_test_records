bde9f6ced74f191d473a15a18ab0d07696389029
mswdwk <mswdwk@outlook.com>
2022-01-21 12:23:31 -0500

    feat: add pgsql sysbench test

--- mysql-exercise/sysbench/test1/action.sh
+++ mysql-exercise/sysbench/test1/action.sh
@@ -1,1 +1,4 @@
- sysbench /usr/share/sysbench/oltp_read_write.lua --time=60 --mysql-host=127.0.0.1 --mysql-port=3310 --mysql-password='Roo123..' --mysql-user=root  --mysql-db=testdb --table-size=100000 --tables=10 --threads=10 $* 
+# sysbench /usr/share/sysbench/oltp_read_write.lua --time=60 --mysql-host=127.0.0.1 --mysql-port=3310 --mysql-password='Roo123..' --mysql-user=root  --mysql-db=testdb --table-size=100000 --tables=10 --threads=10 $* 
+HOST=192.168.79.133
+PORT=3310
+sysbench /usr/share/sysbench/oltp_read_write.lua --time=60 --mysql-host=$HOST --mysql-port=$PORT --mysql-password='123' --mysql-user=root  --mysql-db=testdb --table-size=100000 --tables=10 --threads=10 $* 
--- mysql-exercise/sysbench/test1/prepare.sh
+++ mysql-exercise/sysbench/test1/prepare.sh
@@ -1,3 +1,5 @@
- sysbench /usr/share/sysbench/oltp_read_write.lua --time=40 --mysql-host=127.0.0.1 --mysql-port=3310 --mysql-password='Roo123..' --mysql-user=root  --mysql-db=testdb --table-size=50000000 --tables=1 --threads=8 --report-interval=5 prepare
+ #sysbench /usr/share/sysbench/oltp_read_write.lua --time=40 --mysql-host=192.168.79.133 --mysql-port=3310 --mysql-password='123' --mysql-user=root  --mysql-db=testdb --table-size=500000 --tables=1 --threads=8 --report-interval=5 prepare
+ sysbench /usr/share/sysbench/oltp_read_write.lua --time=40 --mysql-host=192.168.79.133 --mysql-port=3310 --mysql-password='123' --mysql-user=root  --mysql-db=testdb --table-size=100000 --tables=1 --threads=8 --report-interval=5 prepare
+ #sysbench /usr/share/sysbench/oltp_read_write.lua --time=40 --mysql-host=127.0.0.1 --mysql-port=3310 --mysql-password='Roo123..' --mysql-user=root  --mysql-db=testdb --table-size=50000000 --tables=1 --threads=8 --report-interval=5 prepare
  #sysbench /usr/share/sysbench/oltp_read_write.lua --time=80 --mysql-host=192.168.32.135 --mysql-port=3306 --mysql-user=root --mysql-password=123 --mysql-db=test --table-size=10000 --tables=5 --threads=4 prepare
  #sysbench /usr/share/sysbench/oltp_read_write.lua --time=80 --mysql-host=localhost --mysql-port=3306 --mysql-user=root --mysql-password=123 --mysql-db=test --table-size=10000 --tables=5 --threads=4 prepare
