6edda193c8c19607c2021e62b96b8ff0813c7208
mswdwk <mswdwk@outlook.com>
2022-05-28 16:02:55 +0800

    feat: modify mysql test for go

--- go/hello/hello.go
+++ go/hello/hello.go
@@ -19,24 +19,42 @@ func fibonacci(n int, c chan int) {
 }
 
 func test_mysql() {
-	db, err := sqlx.Open("mysql", "root:123@tcp(192.168.79.133:3310)/testdb")
+	db, err := sqlx.Open("mysql", "root:123456@tcp(192.168.79.132:3310)/testdb")
 	//database, err := sqlx.Open("数据库类型", "用户名:密码@tcp(地址:端口)/数据库名")
 	if err != nil {
-		fmt.Println("open mysql filed: ", err)
+		fmt.Println("open mysql failed: ", err)
 		return
 	} else {
 		fmt.Println("open mysql success")
+	}
+
+    fmt.Println("db:",db);
+	r,err := db.Exec( "create database if not exists testdb ");
+	if err != nil {
+		fmt.Println("create database testdb failed: ", err)
+		return
+	} else {
+		fmt.Println("create database testdb success")
+	}
+
+	r1, err := db.Exec( "create table if not exists user(username varchar(32),sex char(16),email char(32));");
+	if err != nil {
+		fmt.Println("create table user failed: ", err)
+		return
+	} else {
+		fmt.Println("create table user success:",r1)
 	}
 
 	sql := "insert into user(username,sex, email)values (?,?,?)"
 	value := [3]string{"user01", "man", "user01@163.com"}
 
 	//执行SQL语句
-	r, err := db.Exec(sql, value[0], value[1], value[2])
+	r2, err := db.Exec(sql, value[0], value[1], value[2])
 	if err != nil {
 		fmt.Println("exec failed,", err)
 		return
 	}
+	fmt.Println("affect rows:",r2)
 
 	//查询最后一天用户ID，判断是否插入成功
 	id, err := r.LastInsertId()
