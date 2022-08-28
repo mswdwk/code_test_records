94141142ed8e722d4aa0bf96211b46f20e9c6ab2
mswdwk <mswdwk@outlook.com>
2022-08-28 23:06:55 +0800

    feat: add sqlcost for mysql tpch test

--- tpch/mysql/4_run_sql.sh
+++ tpch/mysql/4_run_sql.sh
@@ -6,9 +6,16 @@ fi
 if [ -f tpch_err.txt ];then
 	mv tpch_err.txt tpch_err.txt.old
 fi
+if [ -f tpch_sql_cost.txt ];then
+	mv tpch_sql_cost.txt tpch_sql_cost.txt.old
+fi
 for ((i=1;i<=22;++i));do
+	start_s=`date +%s`
 	echo "[`date`]Test SQL $i" >> tpch_ok.txt
 	time mysql --login-path=$DBUSER -D$DBNAME -e "source queries/d$i.sql"	>> tpch_ok.txt
+	end_s=`date +%s`
+	cost_s=`echo "$end_s - $start_s"|bc`
+	echo "SQL $i cost $cost_s seconds" >> tpch_sql_cost.txt
 	if [ $? -ne 0 ];then
 		echo "[`date`] $i $?" >> tpch_err.log
 	fi
