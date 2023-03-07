7c278bd0b708a31613c38033b61001778f8777bd
mswdwk <mswdwk@outlook.com>
2023-03-07 08:13:47 +0800

    fix: parse sql by c

--- mysql-exercise/sqlcost/main.cc
+++ mysql-exercise/sqlcost/main.cc
@@ -3,14 +3,27 @@
 #include <mysql.h>
 //#include <mysql/services.h>
 #include <mysql/plugin.h>
+#include <sql/set_var.h>
+#include <sql/mysqld.h>
 
-
-// extern  struct mysql_parser_service_st * mysql_parser_service;
-extern int init_common_variables();
-extern 
 int main()
 {
+	// init_common_variables();
+	printf("mysql_parser_service is %p\n",mysql_parser_service);
+	// std::string my_name(my_progname);
+	// char *argv[] = {const_cast<char *>(my_name.c_str()),
+	// 				const_cast<char *>("--secure-file-priv=NULL"),
+	// 				const_cast<char *>("--log_syslog=0"),
+	// 				const_cast<char *>("--explicit_defaults_for_timestamp"),
+	// 				const_cast<char *>("--datadir=" DATA_DIR),
+	// 				const_cast<char *>("--lc-messages-dir=" ERRMSG_DIR),
+	// 				nullptr};
+	// set_remaining_args(6, argv);
+	system_charset_info = &my_charset_utf8mb4_general_ci;
+
+	// mysql_mutex_init(PSI_NOT_INSTRUMENTED, &LOCK_plugin, MY_MUTEX_INIT_FAST);
+	sys_var_init();
 	init_common_variables();
-	// printf("p is %p\n",mysql_parser_service);
+
 	return 0;
 }
