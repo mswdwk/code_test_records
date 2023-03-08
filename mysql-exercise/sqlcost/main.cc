b8002517da7a14d23104ffbfd8b00e1a0524efa7
mswdwk <mswdwk@outlook.com>
2023-03-09 04:04:34 +0800

    feat: sql parse by c , compile ok, run failed

--- mysql-exercise/sqlcost/main.cc
+++ mysql-exercise/sqlcost/main.cc
@@ -1,29 +1,47 @@
 #include<stdio.h>
 // #include "mysql/service_parser.h"
-#include <mysql.h>
 //#include <mysql/services.h>
+#include <zconf.h>
+#include <zlib.h> 
+#include <stddef.h>
+#include <my_inttypes.h>
+#include <sql/current_thd.h>
 #include <mysql/plugin.h>
 #include <sql/set_var.h>
 #include <sql/mysqld.h>
+#include <sql/sql_class.h>
+#include <sql/sql_lex.h>
+#include <sql/sql_parse.h>
+// #include <my_md5_size.h>
 
 int main()
 {
-	// init_common_variables();
-	printf("mysql_parser_service is %p\n",mysql_parser_service);
-	// std::string my_name(my_progname);
-	// char *argv[] = {const_cast<char *>(my_name.c_str()),
-	// 				const_cast<char *>("--secure-file-priv=NULL"),
-	// 				const_cast<char *>("--log_syslog=0"),
-	// 				const_cast<char *>("--explicit_defaults_for_timestamp"),
-	// 				const_cast<char *>("--datadir=" DATA_DIR),
-	// 				const_cast<char *>("--lc-messages-dir=" ERRMSG_DIR),
-	// 				nullptr};
-	// set_remaining_args(6, argv);
-	system_charset_info = &my_charset_utf8mb4_general_ci;
+	THD *thd = new THD(false);
+    const char *query_text = "select 1";
+    uint query_length = strlen(query_text);
+    Object_creation_ctx *ctx = nullptr;
+    bool rc;
 
-	// mysql_mutex_init(PSI_NOT_INSTRUMENTED, &LOCK_plugin, MY_MUTEX_INIT_FAST);
-	sys_var_init();
-	init_common_variables();
+    Parser_state parser_state;
+    if (parser_state.init(thd, query_text, query_length))
+    {
+		printf("error\n");
+    }
 
+    parser_state.m_input.m_compute_digest= true;
+
+    rc= parse_sql(thd, &parser_state, ctx);
+    if (! rc)
+    {
+      // unsigned char md5[MD5_HASH_SIZE];
+      char digest_text[1024];
+      bool truncated;
+      const sql_digest_storage *digest= & thd->m_digest->m_digest_storage;
+
+      // compute_digest_md5(digest, & md5[0]);
+      // compute_digest_text(digest, & digest_text[0], sizeof(digest_text), &truncated);
+	  printf("parse sql OK\n");
+    }
+	
 	return 0;
 }
