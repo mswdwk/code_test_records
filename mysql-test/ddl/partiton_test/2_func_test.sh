bcd8c7acca6815db380914c157d0d352c6fe85cb
mswdwk <mswdwk@outlook.com>
2023-07-07 09:12:37 +0800

    feat: add mysql test suite main script

--- mysql-test/ddl/partiton_test/2_func_test.sh
+++ mysql-test/ddl/partiton_test/2_func_test.sh
@@ -4,4 +4,4 @@ mkdir -p /tmp/qa/test
 mkdir -p /tmp/qa/logs
 #perl mysql-test-run.pl --extern user=$USER --extern password=$PASSWORD --extern host='localhost' --extern port=3310 funcs_1.ddl_t
 cd $MYSQL_TEST
-perl $MYSQL_TEST/mysql-test-run.pl --record --extern socket=$BASE/bin/mysql1.sock --extern password=$PASSWORD  $SUITE_CASE.$TEST_CASE
+perl $MYSQL_TEST/mysql-test-run.pl --user=$USER --record --extern socket=$BASE/bin/mysql1.sock --extern password=$PASSWORD  --extern port=$MYSQL_PORT $SUITE_CASE.$TEST_CASE
