6dd8ac9d5f02774e059345ea25d5c76100a1aff0
mswdwk <mswdwk@outlook.com>
2023-07-17 20:32:54 +0800

    refactor: manual launch tidb

---
+++ tidb_hbase/0_vars.sh
@@ -0,0 +1,11 @@
+export PD_DIR=~/Repos/pingcap/pd/bin
+export PD_DATA_DIR=/tmp/pingcap/pd
+export PD_LOG_DIR=/tmp/pingcap/log
+
+export KV_DIR=~/Repos/pingcap/tikv/target/release
+export KV_DATA_DIR=/tmp/pingcap/tikv
+export KV_LOG_DIR=/tmp/pingcap/log
+
+export TIDB_DIR=~/Repos/pingcap/tidb/bin
+export TIDB_DATA_DIR=/tmp/pingcap/tidb
+export TIDB_LOG_DIR=/tmp/pingcap/log
--- tidb_hbase/start_tikv.sh
+++
@@ -1,1 +0,0 @@
-./tikv-server --pd-endpoints="127.0.0.1:2379" --addr="127.0.0.1:20160" --data-dir=tikv1 --log-file=tikv1.log &
