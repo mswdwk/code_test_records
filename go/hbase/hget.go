5419f537edc078beba210de05366efe7f4c7a345
mswdwk <mswdwk@outlook.com>
2023-07-18 11:24:43 +0800

    feat: add hbase create/drop table , put row test

--- go/hbase/hget.go
+++ go/hbase/hget.go
@@ -1,44 +1,17 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/tsuna/gohbase"
-	"github.com/tsuna/gohbase/hrpc"
+	"flag"
+	"os"
 )
 
+var host = flag.String("host", "localhost:2181", "The location where HBase is running")
+
 func main() {
-	// ip:port 是hbase 连接的 etcd/zkServer
-	hbase_client := gohbase.NewClient("192.168.79.134:12181") // hbase服务器地址,可以是只有ip没有port，也可以是ip:port。
-	// hbase_client := gohbase.NewClient("127.0.0.1")
-	getRequest, err := hrpc.NewGetStr(context.Background(), "student", "00000000000000000009512345")
-	getRsp, err := hbase_client.Get(getRequest) // Get()方法返回查询结果。通过客户端真正读取数据
-	hbase_client.Put(getRequest)
-	adm_cli := gohbase.NewAdminClient("")
-	adm_cli.del
-	if err != nil {
-		fmt.Println("hbase get client error:" + err.Error())
-		return
-	}
-	// create 'member','member_id','address','info'
-	type mystruct struct {
-		Use    string               `json:"user_id" `
-		Movies map[string][]float64 `json:"movies" ` // 用户看的多部电影 "电影id":[打分int,喜好程度float]
-	}
-	for k, v := range getRsp.Cells { // v结构体中的Value保存了真正的数据
-		// value := v.Value
-		fmt.Println("key:" + string(k))
-		//fmt.Printf("v=%V"+ *v)
-		fmt.Println("Row:" + string(v.Row))
-		fmt.Println("Family:" + string(v.Family))
-		fmt.Println("Qualifier:" + string(v.Qualifier))
-		fmt.Println("value:" + string(v.Value))
-		//      var myuser mystruct
-		//      err := json.Unmarshal(value, &myuser) // value为 []unit8类型的字节数组，所以可以直接放到json.Unmarshal
-		//      if err != nil {
-		//              fmt.Println(err.Error())
-		//      }
-		//      fmt.Println(myuser)
-	}
+	flag.Parse()
+	TestTableOpMain(false)
+	TestGetMain()
+	TestPutMain()
+	TestDeleteMain()
+	os.Exit(0)
 }
