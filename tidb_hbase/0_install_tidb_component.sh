a3f2c85cbb009ee22f7ac38543dace606ef59292
mswdwk <mswdwk@outlook.com>
2023-10-20 19:37:59 +0800

    fix: let it go on MacOS for tidb_hbase

---
+++ tidb_hbase/0_install_tidb_component.sh
@@ -0,0 +1,4 @@
+tiup list pd
+
+tiup install pd:v6.6.0
+tiup install tikv:v6.6.0
--- tidb_hbase/0_vars.sh
+++ tidb_hbase/0_vars.sh
@@ -4,17 +4,20 @@ if [ $OS = "GNU/Linux" ];then
 	export PD_DIR=~/repos/pingcap/pd/bin
 	export KV_DIR=~/repos/pingcap/tikv/target/release
 	export TIDB_DIR=~/repos/pingcap/tidb/bin
+	export KV_DATA_DIR=/data/pingcap/tikv
+	export HBASE_PATH="127.0.0.1:12181"
 else 
 	export PD_DIR=~/.tiup/components/pd/v6.6.0
 	export KV_DIR=~/repos/pingcap/tikv/target/release
 	export TIDB_DIR=~/repos/pingcap/tidb/bin
 	#echo "Not support $OS"
+	export KV_DATA_DIR=/tmp/pingcap/tikv
+	export HBASE_PATH="127.0.0.1:2181"
 fi
 
 export PD_DATA_DIR=/tmp/pingcap/pd
 export PD_LOG_DIR=/tmp/pingcap/log
 
-export KV_DATA_DIR=/data/pingcap/tikv
 export KV_LOG_DIR=/tmp/pingcap/log
 
 export TIDB_DATA_DIR=/tmp/pingcap/tidb
