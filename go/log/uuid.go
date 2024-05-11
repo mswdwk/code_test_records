dd8e48381d35f43003a6153c789e848dc2fdf815
mswdwk <mswdwk@outlook.com>
2024-05-11 10:28:44 +0800

    feat: uuid test

--- go/log/uuid.go
+++ go/log/uuid.go
@@ -9,9 +9,9 @@ import (
 
 // Create a Version 4 UUID, panicking on error.
 // Use this form to initialize package-level variables.
-var u1 = uuid.Must(uuid.NewV4())
+var _ = uuid.Must(uuid.NewV4())
 
-func main() {
+func test_uuid_v4() {
 	// Create a Version 4 UUID.
 	u2, err := uuid.NewV4()
 	if err != nil {
@@ -26,4 +26,69 @@ func main() {
 		log.Fatalf("failed to parse UUID %q: %v", s, err)
 	}
 	log.Printf("successfully parsed UUID %v", u3)
+}
+
+func test_uuid_v1() {
+	// Create a Version 4 UUID.
+	u2, err := uuid.NewV1()
+	if err != nil {
+		log.Fatalf("failed to generate UUID: %v", err)
+	}
+	log.Printf("generated Version 1 UUID %v", u2)
+
+	// Parse a UUID from a string.
+	s := "6ba7b810-9dad-11d1-80b4-00c04fd430c8"
+	u3, err := uuid.FromString(u2.String())
+	if err != nil {
+		log.Fatalf("failed to parse UUID %q: %v", s, err)
+	}
+	log.Printf("successfully parsed UUID %v", u3)
+}
+
+func tmap() *map[string]int {
+	log.Printf("return map ")
+	a := make(map[string]int)
+	a["key"] = 123
+	return &a
+}
+
+func test_map1() {
+	a := tmap()
+	var k = "key"
+	if nil != a {
+		b := *a
+		if v, ok := b[k]; ok {
+			log.Printf("v= %v\n", v)
+		} else {
+			log.Printf("not found key: %s\n", k)
+		}
+	}
+}
+
+func tmap2() *map[string]*int {
+	log.Printf("return map 2")
+	m := make(map[string]*int)
+	var b = 1234567890
+	m["key"] = &b
+	return &m
+}
+
+func test_map2(k string) {
+	a := tmap2()
+	if nil != a {
+		m := *a
+		if v, ok := m[k]; ok {
+			log.Printf("v= %v\n", *v)
+		} else {
+			log.Printf("not found key: %s\n", k)
+		}
+	}
+}
+
+func main() {
+	test_uuid_v1()
+	test_uuid_v4()
+	test_map1()
+	test_map2("key")
+	test_map2("key2")
 }
