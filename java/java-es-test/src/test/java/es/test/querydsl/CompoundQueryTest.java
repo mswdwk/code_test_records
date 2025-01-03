cb8500c597090211d90d4cc0a2806cde536702b4
mswdwk <mswdwk@outlook.com>
2025-01-03 21:38:14 +0800

    feat: add es test

---
+++ java/java-es-test/src/test/java/es/test/querydsl/CompoundQueryTest.java
@@ -0,0 +1,70 @@
+package es.test.querydsl;
+
+import es.test.BaseTest;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.index.query.ConstantScoreQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.DeleteByQueryAction;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+// import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.elasticsearch.index.query.QueryBuilders.constantScoreQuery;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.elasticsearch.xcontent.XContentFactory.jsonBuilder;
+
+@Slf4j
+public class CompoundQueryTest extends BaseTest {
+
+
+  @Before
+  public void setUp() throws IOException {
+
+    CreateIndexRequest req = new CreateIndexRequest("test");
+
+    client.admin().indices().prepareCreate("test");
+    //初始化测试数据
+    bulkProcessor.add(new IndexRequest("test", "test").source(jsonBuilder().startObject().field("name", "王百万").endObject()));
+    bulkProcessor.add(new IndexRequest("test", "test").source(jsonBuilder().startObject().field("name", "王大力").endObject()));
+    bulkProcessor.add(new IndexRequest("test", "test").source(jsonBuilder().startObject().field("name", "赵海洋").endObject()));
+    bulkProcessor.add(new IndexRequest("test", "test").source(jsonBuilder().startObject().field("name", "丁建国").endObject()));
+    bulkProcessor.flush();
+    bulkProcessor.close();
+    client.admin().indices().prepareRefresh().get();
+
+  }
+
+  @After
+  public void tearDown() {
+    //删除测试数据
+    /*DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
+            .filter(QueryBuilders.matchAllQuery())
+            .source("test")
+            .get();*/
+    DeleteIndexRequest req = new DeleteIndexRequest( );
+    req.indices("test");
+    client.admin().indices().delete(req);
+  }
+
+  @Test
+  public void constantScore() {
+    ConstantScoreQueryBuilder builder =
+            constantScoreQuery(
+                    termQuery("name", "王百万"))
+                    .boost(1.0f);
+    log.info(builder.toString());
+    SearchResponse searchResponse = client.prepareSearch().setQuery(builder).get();
+    log.info(searchResponse.toString());
+    //Assert.assertEquals(1, searchResponse.getHits().getTotalHits());
+  }
+}
