d8f9dc0306749a4e8a9d3f6241e684575572c109
mswdwk <mswdwk@outlook.com>
2025-03-01 22:19:20 +0800

    feat: get jobname.

--- java/spring/gs-batch-processing/complete/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ java/spring/gs-batch-processing/complete/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -7,9 +7,12 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BatchProcessingApplication implements CommandLineRunner {
@@ -25,6 +28,9 @@ public class BatchProcessingApplication implements CommandLineRunner {
 	@Autowired
 	private Job importPersonJob;
 
+	@Value("${spring.batch.job.name}")
+	private String jobname;
+
 	public static void main(String[] args) {
 		log.info("main ");
 		System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessingApplication.class, args)));
@@ -32,6 +38,9 @@ public class BatchProcessingApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		log.info("cmd , jobname "+jobname);
+		log.info("args length: " +args.length);
+		log.info("args : {}" , args);
 		log.info("Registered Jobs: " + jobRegistry.getJobNames());
 		jobLauncher.run(importPersonJob,new JobParameters());
 	}
