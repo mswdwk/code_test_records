5cd11f1aa466ce599a23a62074e350565b1be3b9
mswdwk <mswdwk@outlook.com>
2025-06-08 15:44:46 +0800

    feat: add mybatis modify sql demo

---
+++ java/mybatis3-demo2/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception {
+        System.out.println( "Hello World!" );
+
+        MyBatisSqlModifier.tmain(null);
+    }
+}
--- java/mybatis3-demo2/src/main/java/org/example/mapper/UserMapper.java
+++ java/mybatis3-demo2/src/main/java/org/example/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface UserMapper {
     List<User> selectAll();
     User selectById(Long id);
-    void insert(User user);
-    void update(User user);
-    void delete(Long id);
+//    void insert(User user);
+//    void update(User user);
+//    void delete(Long id);
 }
