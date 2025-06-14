cdb84098ff1201d83c285705e888e7b8191634d7
mswdwk <mswdwk@outlook.com>
2025-06-14 21:37:30 +0800

    feat: add a task view vue

---
+++ vue/myweb/src/views/TaskView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="task">
+    <h1>This is an task page</h1>
+     <button @click="count++">
+    Count is: {{ count }}
+  </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
