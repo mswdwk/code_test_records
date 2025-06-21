32ecee03a9026e7c8e40046aa8cf56842a4a0c86
mswdwk <mswdwk@outlook.com>
2025-06-21 16:31:20 +0800

    add vue demo

--- vue/myweb/src/views/TaskView.vue
+++ vue/myweb/src/views/TaskView.vue
@@ -4,14 +4,59 @@
      <button @click="count++">
     Count is: {{ count }}
   </button>
+  <input name="id"></input>
+
+   <p>Message is: {{ message }}</p>
+    <input v-model="message" placeholder="edit me" />
+
+   <div>Checked names: {{ checkedNames }}</div>
+
+<input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+<label for="jack">Jack</label>
+
+<input type="checkbox" id="john" value="John" v-model="checkedNames" />
+<label for="john">John</label>
+
+<input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+<label for="mike">Mike</label>
+<tr>new line</tr>
+<p></p>
+
   </div>
+  <div>Picked: {{ picked }}
+
+<input type="radio" id="one" value="One" v-model="picked" />
+<label for="one">One</label>
+
+<input type="radio" id="two" value="Two" v-model="picked" />
+<label for="two">Two</label>
+</div>
+
+<div>Selected: {{ selected }}</div>
+
+<select v-model="selected">
+  <option disabled value="">Please select one</option>
+  <option>A</option>
+  <option>B</option>
+  <option>C</option>
+</select>
+   
+<li v-for="item in items">
+ 你好呀 {{ item.message }}
+</li>
+  
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 0
+      count: 0,
+      checkedNames: [],
+      message:"",
+      picked:"One",
+      selected:null,
+        items: [{ message: 'Foo' }, { message: 'Bar' }]
     }
   }
 }
@@ -26,7 +71,7 @@ button {
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .task {
     min-height: 100vh;
     display: flex;
     align-items: center;
