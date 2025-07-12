5acea3bc76b29ead55f48ad9b791720e553af0c1
mswdwk <mswdwk@outlook.com>
2025-07-12 14:34:59 +0800

    chore: adjust

--- java/spring/springboot3demo/src/main/java/com/example/demo/batch/JobCompletionNotificationListener.java
+++ java/spring/springboot3demo/src/main/java/com/example/demo/batch/JobCompletionNotificationListener.java
@@ -6,7 +6,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -19,9 +18,9 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 
     // @Autowired
     // @Qualifier("secondaryJdbcTemplate")
-    private JdbcTemplate jdbcTemplate;
+    final private JdbcTemplate jdbcTemplate;
 
-    public JobCompletionNotificationListener(@Qualifier("secondaryJdbcTemplate")JdbcTemplate jdbcTemplate) {
+    public JobCompletionNotificationListener(@Qualifier("secondaryJdbcTemplate") JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -33,7 +32,7 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 
             jdbcTemplate
                     .query("SELECT first_name, last_name FROM people2", new DataClassRowMapper<>(Person.class))
-                    .forEach(person -> log.info("Found persion <{}> in the database.",person.firstName()));
+                    .forEach(person -> log.info("Found persion <{}> in the database.", person.firstName()));
         }
     }
 }
